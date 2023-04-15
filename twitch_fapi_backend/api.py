31f25fafc5b9ed93a072c7d8017aa37233f21cdf
DavidVentura <davidventura27@gmail.com>
2023-04-15 13:06:40 +0200

    add mqtt publish for cec control

--- twitch_fapi_backend/api.py
+++ twitch_fapi_backend/api.py
@@ -1,10 +1,14 @@
 import asyncio
 import logging
 import sys
+import enum
 
 import aiocache
 import uvicorn
 
+import asyncio_mqtt
+
+from contextlib import asynccontextmanager
 from typing import Optional
 
 from twitch_fapi_backend.twitch import Twitch
@@ -19,21 +23,52 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger()
 cache = aiocache.SimpleMemoryCache()
 t = Twitch(settings.CLIENT_ID, settings.CLIENT_SECRET)
-app = FastAPI()
+mqtt_client = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global mqtt_client
+    asyncio.create_task(t.get_token_forever())
+    asyncio.create_task(tasks.store_progress())
+    while not t.ready:
+        await asyncio.sleep(0.1)
+
+    async with asyncio_mqtt.Client(hostname=settings.MQTT_HOST) as c:
+        mqtt_client = c
+        yield
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+
+class Power(str, enum.Enum):
+    ON = "on"
+    OFF = "off"
+
+
+class Inputs(str, enum.Enum):
+    KODI = "kodi"
+    CHROMECAST = "chromecast"
+
+
+class CecCommands(str, enum.Enum):
+    POWER_ON = "POWER_ON"
+    POWER_OFF = "POWER_OFF"
+    SOURCE_HDMI_1 = "SOURCE_HDMI_1"
+    SOURCE_HDMI_2 = "SOURCE_HDMI_2"
+    SOURCE_HDMI_3 = "SOURCE_HDMI_3"
+    SOURCE_HDMI_4 = "SOURCE_HDMI_4"
+
+
+INPUT_MAPPING = {Inputs.KODI: CecCommands.SOURCE_HDMI_4,
+                 Inputs.CHROMECAST: CecCommands.SOURCE_HDMI_3,
+                 }
 
 
 @app.get("/")
 async def root():
     return {"message": "Hi"}
-
-
-@app.on_event("startup")
-async def on_startup():
-    asyncio.create_task(t.get_token_forever())
-    asyncio.create_task(tasks.store_progress())
-    while not t.ready:
-        await asyncio.sleep(0.1)
 
 @app.get("/list")
 async def list_streams():
@@ -81,11 +116,30 @@ async def currently_casting():
     return got or playing
 
 
+@app.get("/remote/input/{input}")
+async def change_input(input: Inputs):
+    res = await publish(INPUT_MAPPING[input])
+    return {}
+
+
+@app.get("/remote/tv/{power}")
+async def tv_power(power: Power):
+    if power is Power.ON:
+        res = await publish(CecCommands.POWER_ON)
+    elif power is Power.OFF:
+        res = await publish(CecCommands.POWER_OFF)
+    return {}
+
+async def publish(comm: CecCommands):
+    logger.info("Publishing %s to %s", comm.value, settings.CEC_TOPIC)
+    res = await mqtt_client.publish(settings.CEC_TOPIC, comm.value)
+    logger.info("Got %s", res)
+    return res
+
 @app.get("/end")
 async def end():
     await kodi.stop_playing()
     return None
-
 
 def main():
     uvicorn.run(app, port=7777, host='0.0.0.0')
